0c37a9a6f507a661a8d7e58b91b886a9097ed0fc
KAM374 <kerri.marshall12@gmail.com>
2026-03-13 18:11:30 -0400

    Create app.py
    adding the code for the to-do list application and adding the correct host and port

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__ToDoList__)
+@app.route('/')
+
+tasks=[]
+print("To-Do List")
+ 
+while True:
+
+  i=0
+  i=i+1
+  action = input("Type 'add', 'delete', 'quit': ")
+
+  if action == "add":
+    task = input("enter a task: ")
+    tasks.append(task)
+    
+  elif action == "delete":
+    num = int(input("enter the task number to delete: "))
+    tasks.pop(num-1)
+    print("Your To-Do List \n",  tasks)
+    
+  elif  action == "quit":
+    print("Your To-Do List \n",  tasks)
+     
+if __ToDoList__ == '__main__':
+  app.run(debug=True, host="0.0.0.0", port=8080)
